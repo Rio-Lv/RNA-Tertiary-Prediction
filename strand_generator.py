90077593a0f3402c0d05e0c23ebdb51859cceec0
riolv <wong.rogerio@gmail.com>
2025-03-23 10:17:05 +0000

    nucleotide given coordinate

--- strand_generator.py
+++ strand_generator.py
@@ -6,14 +6,15 @@ from pydantic import BaseModel
 import random
 
 
-class Nucleotide(BaseModel):
-    type: Literal["A", "C", "G", "U"]
-
-
 class Coordinate(BaseModel):
     x: float
     y: float
     z: float
+
+
+class Nucleotide(BaseModel):
+    type: Literal["A", "C", "G", "U"]
+    coordinate: Coordinate = Coordinate(x=0, y=0, z=0)
 
 
 def generate_rand_coord(range=100) -> Coordinate:
@@ -35,17 +36,23 @@ def generate_random_strand(sequence: Sequence, id="generate_strand") -> Strand:
         z_1=[coord.z for coord in coordinates],
     )
 
+
 def generate_line_strand(sequence: Sequence, id="generate_strand") -> Strand:
-    nucleotides: list[Nucleotide] = [Nucleotide(type=nt) for nt in sequence.sequence]
-    coordinates: list[Coordinate] = [Coordinate(x=0, y=0, z=i*6.5) for i in range(1, len(nucleotides) + 1)]
+    nucleotides: list[Nucleotide] = [
+        Nucleotide(type=nt, coordinate=Coordinate(x=0, y=0, z=i * 6.5))
+        for i, nt in enumerate(sequence.sequence)
+    ]
     return Strand(
         ID=[id + f"_{i}" for i in range(1, len(nucleotides) + 1)],
         resname=[nt.type for nt in nucleotides],
         resid=[i for i in range(1, len(nucleotides) + 1)],
-        x_1=[coord.x for coord in coordinates],
-        y_1=[coord.y for coord in coordinates],
-        z_1=[coord.z for coord in coordinates],
+        x_1=[nt.coordinate.x for nt in nucleotides],
+        y_1=[nt.coordinate.y for nt in nucleotides],
+        z_1=[nt.coordinate.z for nt in nucleotides],
     )
+
+
+# def displace_nucleotide()
 
 if __name__ == "__main__":
     print("Generating Strand")
@@ -58,4 +65,6 @@ if __name__ == "__main__":
     strand = generate_line_strand(sequence)
     # 4. Save to Examine PDB
     pdb = strand_to_pdb(strand)
+    print(pdb)
     save_pdb(pdb, f"data/pdbs/strand_generator_test_{pdb_id}.pdb")
+    print("Saved PDB")
