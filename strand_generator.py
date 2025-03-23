9d6530a82fba4b6ce129ade924bd9968ed07ca4e
riolv <wong.rogerio@gmail.com>
2025-03-23 21:45:39 +0000

    added handling to the store -1e+18 when missing value

--- strand_generator.py
+++ strand_generator.py
@@ -95,25 +95,41 @@ def orbital_pull(
     radii_dict = OrbitalRadii().radii
     radii = radii_dict.get(orbital_type, None)
     if radii is None:
-        index_1 = nucleotide_1.index
-        index_2 = nucleotide_2.index
-        if index_1 == index_2:
-            return
-        if index_1 - index_2 == 1 or index_2 - index_1 == 1:
-            radii = 6.5
-        else:
-            return
+        return
+    
     distance = distance_matrix[nucleotide_1.index][nucleotide_2.index]
     if distance < radii:
         k = -k
-    force = k * min( 1/abs(distance - radii)**2, 0.1)
+    
+    force = k * min( 1/abs(distance - radii)**2, 0.4)
+    
     ux = (nucleotide_2.coordinate.x - nucleotide_1.coordinate.x) / distance
     uy = (nucleotide_2.coordinate.y - nucleotide_1.coordinate.y) / distance
     uz = (nucleotide_2.coordinate.z - nucleotide_1.coordinate.z) / distance
     nucleotide_1.coordinate.x += force * ux + random.uniform(-noise, noise)
     nucleotide_1.coordinate.y += force * uy + random.uniform(-noise, noise)
     nucleotide_1.coordinate.z += force * uz + random.uniform(-noise, noise)
-    
+
+def correct_bonds(nucleotides: list[Nucleotide]):
+    for i in range(1, len(nucleotides)):
+        nt1 = nucleotides[i-1]
+        nt2 = nucleotides[i]
+        distance = (
+            (nt1.coordinate.x - nt2.coordinate.x) ** 2
+            + (nt1.coordinate.y - nt2.coordinate.y) ** 2
+            + (nt1.coordinate.z - nt2.coordinate.z) ** 2
+        ) ** 0.5
+        d = distance - 6.5
+        ux = (nt2.coordinate.x - nt1.coordinate.x) / distance
+        uy = (nt2.coordinate.y - nt1.coordinate.y) / distance
+        uz = (nt2.coordinate.z - nt1.coordinate.z) / distance
+        dx = ux * d
+        dy = uy * d
+        dz = uz * d
+        for nt in nucleotides[i:]:
+            nt.coordinate.x -= dx
+            nt.coordinate.y -= dy
+            nt.coordinate.z -= dz
 
 
 def displace_nucleotide(
@@ -132,6 +148,7 @@ def crude_simulate(
         for nt in nucleotides:
             neighbors = get_nearest_nucleotides(N, nt, nucleotides, distance_matrix)
             displace_nucleotide(nt, neighbors, distance_matrix, k=k)
+        correct_bonds(nucleotides)
     return nucleotides
 
 
@@ -168,11 +185,11 @@ if __name__ == "__main__":
 
     # 5. Try out Crude Simulation
     print("Crude Simulation")
-    N = 10 # number of nearest neighbors
+    N = 20 # number of nearest neighbors
     nucleotides = crude_simulate(N, nucleotides, k=2, steps=200)
     strand = nucleotides_to_strand(nucleotides)
     pdb = strand_to_pdb(strand)
     print(pdb)
-    path = f"data/pdbs/strand_generator_simulation_{pdb_id}.pdb"
+    path = f"data/pdbs/strand_generator_simulation_{pdb_id}_200.pdb"
     save_pdb(pdb, path)
     print("Saved PDB to: ", path)
--- tools.py
+++ tools.py
@@ -16,8 +16,10 @@ class Labels(BaseModel):
             "z_1": [3.4, 4.5, 5.6],
         }
     )
+
+
 class Sequences(BaseModel):
-    
+
     model_config = ConfigDict(arbitrary_types_allowed=True)
     df: pd.DataFrame = pd.DataFrame(
         {
@@ -25,9 +27,14 @@ class Sequences(BaseModel):
             "sequence": ["GGGU", "GGGA", "GAGU"],
             "temporal_cutoff": ["1995-01-26", "1995-01-26", "1995-01-26"],
             "description": ["description1", "description2", "description3"],
-            "all_sequences": [">1SCL_1|Chain A|RN...", ">1RNK_1|Chain A|RN...", ">1RHT_1|Chain A|RN..."],
+            "all_sequences": [
+                ">1SCL_1|Chain A|RN...",
+                ">1RNK_1|Chain A|RN...",
+                ">1RHT_1|Chain A|RN...",
+            ],
         }
     )
+
 
 # ============= Strand Class =============
 class Strand(BaseModel):
@@ -38,17 +45,19 @@ class Strand(BaseModel):
     y_1: list[float] = [7.8, 9.1]
     z_1: list[float] = [3.4, 4.5]
 
+
 class Sequence(BaseModel):
     target_id: str = "1SCL_A"
     sequence: str = "GGGU"
     temporal_cutoff: str = "1995-01-26"
     description: str = "description1"
     all_sequences: str = ">1SCL_1|Chain A|RN..."
-    
+
 
 # ============= Grab Strand =============
 def grab_strand(pdb_id: str, labels: Labels) -> Strand:
     filtered_df = labels.df[labels.df["ID"].str.contains(pdb_id)]
+
     return Strand(
         **{
             field: filtered_df[field].tolist()
@@ -56,6 +65,8 @@ def grab_strand(pdb_id: str, labels: Labels) -> Strand:
             if field in filtered_df.columns
         }
     )
+
+
 def grab_sequence(pdb_id: str, sequences: Sequences) -> Sequence:
     filtered_df = sequences.df[sequences.df["target_id"] == pdb_id]
     return Sequence(
@@ -66,11 +77,31 @@ def grab_sequence(pdb_id: str, sequences: Sequences) -> Sequence:
         }
     )
 
+
 # ============= Strand to PDB =============
 def strand_to_pdb(strand: Strand) -> str:
     pdb = ""
     for i in range(len(strand.ID)):
-        pdb += f"ATOM  {i+1:5}  CA  {strand.resname[i]:3} A{strand.resid[i]:4}    {strand.x_1[i]:8.3f}{strand.y_1[i]:8.3f}{strand.z_1[i]:8.3f}  1.00  0.00\n"
+        # empty values are currently stored as -1e+18
+        # if x y or z are < -1e+6, replace all 3 with space
+        index = f"{i+1:5}"
+        ca= "CA"
+        resname = f"{strand.resname[i]:3}"
+        resid = f"A{strand.resid[i]:4}"
+        x_1 = strand.x_1[i]
+        y_1 = strand.y_1[i]
+        z_1 = strand.z_1[i]
+   
+        suffix = "1.00  0.00"
+        
+        if x_1 < -1e6 or y_1 < -1e6 or z_1 < -1e6:
+            continue
+        
+        x_1 = f"{strand.x_1[i]:8.3f}"
+        y_1 = f"{strand.y_1[i]:8.3f}"
+        z_1 = f"{strand.z_1[i]:8.3f}"
+        
+        pdb += f"ATOM  {index}  {ca}  {resname} {resid}    {x_1} {y_1} {z_1} {suffix}\n"
     return pdb
 
 
@@ -78,17 +109,20 @@ def strand_to_pdb(strand: Strand) -> str:
 def save_pdb(pdb: str, filename: str):
     with open(filename, "w") as f:
         f.write(pdb)
-        
+
+
 # ============= Compute Similarity =============
 def compute_similarity(path_1: str, path_2: str):
     # use USalign to compute similarity
     os.system(f"USalign/USalign {path_1} {path_2}")
 
+
 # ============= Main/Test=============
 if __name__ == "__main__":
-    pdb_id = "2LKR_A"
-    labels = Labels(df=pd.read_csv("data/train_labels.csv"))
-    sequences = Sequences(df=pd.read_csv("data/train_sequences.csv"))
+    # pdb_id = "2LKR_A" # exists in training_labels.csv
+    pdb_id = "R1116"  # exists in validation_labels.csv
+    labels = Labels(df=pd.read_csv("data/validation_labels.csv"))
+    sequences = Sequences(df=pd.read_csv("data/validation_sequences.csv"))
     # labels = Labels()
     # strand = Strand()
     strand = grab_strand(pdb_id, labels)
@@ -96,12 +130,13 @@ if __name__ == "__main__":
     # print(labels)
     # print(strand)
     print(sequence)
-    
+
     pdb = strand_to_pdb(strand)
     print(pdb)
-    save_pdb(pdb, f"data/pdbs/tools_test_{pdb_id}.pdb")
-    print("saved")
-    
-    path_1 = f"data/pdbs/tools_test_{pdb_id}.pdb"
-    path_2 = f"data/pdbs/reference_108d.pdb"
+    path = f"data/pdbs/tools_test_{pdb_id}.pdb"
+    save_pdb(pdb, path)
+    print("saved to: ", path)
+
+    path_1 = f"data/pdbs/strand_generator_simulation_2LKR_A.pdb"
+    path_2 = f"data/pdbs/tools_test_R1116.pdb"
     compute_similarity(path_1, path_2)
