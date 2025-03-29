1e10050869a549bc7bdea343115562c993eb10c2
riolv <wong.rogerio@gmail.com>
2025-03-29 15:26:39 +0000

    mapped real indices to fake after generation because real has missing coords

--- src/generate_data.py
+++ src/generate_data.py
@@ -9,22 +9,27 @@ import random
 
 # =================== Generate Data ===================
 def pdb_id_to_clusters(pdb_id:str, sequences:Sequences,labels:Labels )-> Tuple[list[Nucleotide], list[Nucleotide]]:
-    # 1. Use Sequences to Generate Fake Nucleotides
+
+    max_nucleotides = 300
+    # 1. Use Labels to Generate Real Nucleotides
+    strand = grab_strand(pdb_id, labels)
+    real_nucleotides = strand_to_nucleotides(strand)[:max_nucleotides]
+    real_indices = [nt.index for nt in real_nucleotides]
+    
+    # 2. Use Sequences to Generate Fake Nucleotides
     sequence = grab_sequence(pdb_id, sequences)
     assert len(sequence.sequence) > 5, "Sequence should be longer than 5"
-    fake_nucleotides = sequence_to_nucleotide_line(sequence)[:500]
+    fake_nucleotides = sequence_to_nucleotide_line(sequence)[:max_nucleotides]
     fake_nucleotides = make_fake_nucleotides(5, fake_nucleotides, k=2, steps=100)
-
-    # 2. Use Labels to Generate Real Nucleotides
-    strand = grab_strand(pdb_id, labels)
-    real_nucleotides = strand_to_nucleotides(strand)[:500]
-    
-    # 3. Generate Distance Matrix
-    distance_matrix_fake = calculate_distance_matrix(fake_nucleotides)
-    distance_matrix_real = calculate_distance_matrix(real_nucleotides)
-    # 4. Generate Clusters
+    fake_nucleotides = [nt for nt in fake_nucleotides if nt.index in real_indices]
     
     assert len(fake_nucleotides) == len(real_nucleotides), "Fake and Real Nucleotides should be the same length"
+    
+    # 3. Generate Distance Matrix
+    distance_matrix_real = calculate_distance_matrix(real_nucleotides)
+    distance_matrix_fake = calculate_distance_matrix(fake_nucleotides)
+    # 4. Generate Clusters
+    
     fake_clusters = []
     for i in range(len(fake_nucleotides)):
         nearest_nucleotides = get_nearest_nucleotides(
@@ -68,8 +73,7 @@ def generate_data(n_sequences:int = 5) -> EvaluatorDataset:
     
     curr_index = 1
     for pdb_id in pdb_ids[:n_sequences]:
-        print(f"Generating data for {pdb_id} ({curr_index}/{n_sequences})")
-        curr_index += 1
+        print(f"Generating data for {pdb_id} ({curr_index}/{n_sequences}) ")
         if len(pdb_id) > 5:
             real_clusters, fake_clusters = pdb_id_to_clusters(pdb_id, sequences, labels)
             real_clusters_list.extend(real_clusters)
@@ -77,6 +81,7 @@ def generate_data(n_sequences:int = 5) -> EvaluatorDataset:
         else:
             print(f"Skipping {pdb_id} as it is too short")
             continue
+        curr_index += 1
         
     # 6. Create Dataset
     dataset = EvaluatorDataset(real_clusters_list, fake_clusters_list)
