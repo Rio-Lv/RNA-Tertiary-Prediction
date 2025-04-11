ba075aa1fd9ce4c4dfa4edde6f77acd340953ae2
riolv <wong.rogerio@gmail.com>
2025-04-11 09:23:39 +0100

    100 rounds 6 cluster;4Ds, rand = 0.6 , hard = 0.8

--- src/clusters.py
+++ src/clusters.py
@@ -20,16 +20,16 @@ else:
     
 # ---- HYPERPARAMS ----
 DROPOUT = 0.1
-CLUSTER_SIZE = 4
+CLUSTER_SIZE = 6
 BATCH_SIZE = 256
 N_CLUSTERS = 128 # will be like x8 for different cluster generators
 EPOCHS = 10
 N_ROUNDS = 100
 LOSS_CUT_OFF = 0.01
-LR = 0.001  # Can be changed for refinement?
+LR = 0.005  # Can be changed for refinement?
 N_ITER = 4 # number of iterations to apply delta update
 LOAD_PRETRAINED = True
-# LOAD_PRETRAINED = False
+LOAD_PRETRAINED = False
 NOISE_L = 13
 NOISE_M = 6.5
 NOISE_S = 2
--- src/clusters_test.py
+++ src/clusters_test.py
@@ -6,7 +6,7 @@ import os
 
 # ========== TESTING PARAMETERS ==========
 TEST_SIZE = 5000
-CLUSTER_SIZE = 4
+CLUSTER_SIZE = 6
 K = 2 # noise range Angstrom
 
 if __name__ == "__main__":
