5f2e4d128e02d6fddece2faf16d49bfa0f2bb2af
riolv <wong.rogerio@gmail.com>
2025-04-14 14:42:49 +0100

    0.12 max delta 0.12 temperature, seems quite smooth and yields decent results

--- src/Sequence.py
+++ src/Sequence.py
@@ -23,9 +23,9 @@ N_NEAREST_NEIGBORS = 20  # If using n nearest neighbors for adjustment
 MAX_DISTANCE = 30.0  # If using neightbor within distance for adjustment
 USE_NEIGHBORS = True  # If using n nearest neighbors for adjustment
 
-ITERATIONS = 500
-TEMPERATURE = 0
-MAX_DELTA = 0.08 # Essentially cosmic speed limit
+ITERATIONS = 1000
+TEMPERATURE = 0.12
+MAX_DELTA = 0.12 # Essentially cosmic speed limit
 
 LABELS_PATH = "data/train_labels.csv"
 SEQUENCES_PATH = "data/train_sequences.csv"
@@ -248,7 +248,7 @@ class Sequence:
                     dz = self.coords[j].z - self.coords[i].z
 
                     # use gaussian noise for heat
-                    heat = random.gauss(0, TEMPERATURE)*math.sqrt(abs(diff_mat[i][j]))
+                    heat = random.gauss(0, TEMPERATURE)
 
                     dist = Sequence.distance(self.coords[i], self.coords[j]) + heat
                     if dist < MAX_DISTANCE:
@@ -295,7 +295,7 @@ class Sequence:
                     dz = self.coords[j].z - self.coords[i].z
 
                     # use gaussian noise for heat
-                    heat = random.gauss(0, TEMPERATURE)*math.sqrt(abs(diff_mat[i][j]))
+                    heat = random.gauss(0, TEMPERATURE)
 
 
                     dist = Sequence.distance(self.coords[i], self.coords[j]) + heat
